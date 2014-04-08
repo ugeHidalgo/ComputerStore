68b0821d0fc9718a8c8e210e39bbac27c35c8c6e
Uge Hidalgo <ugehidalgo@hotmail.com>
2014-04-08 18:07:34 +0200

    last

---
+++ ComputerStore/Scripts/datePicker.js
@@ -0,0 +1,27 @@
+﻿
+$(function () {
+
+    //Array para dar formato en español
+    $.datepicker.regional['es'] =
+    {
+        closeText: 'Cerrar',
+        prevText: 'Previo',
+        nextText: 'Próximo',
+
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+        'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+        monthStatus: 'Ver otro mes', yearStatus: 'Ver otro año',
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sáb'],
+        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+        dateFormat: 'dd/mm/yy', firstDay: 1,
+        initStatus: 'Selecciona la fecha', isRTL: false
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+
+    //miDate: fecha de comienzo D=días | M=mes | Y=año
+    //maxDate: fecha tope D=días | M=mes | Y=año
+    $(".datePicker").datepicker({ minDate: "-100Y", maxDate: "+1M +10D", changeMonth: true, changeYear: true, showAnim: 'slideDown' });
+});
